e4147aa871a09e93387f0a9986c278fe1fea4273
rov_waff <2710182206@qq.com>
2025-05-09 20:15:01 +0800

    made entity class

---
+++ backend/src/main/java/fun/seterain3913/notruenews/backend/pojo/News.java
@@ -0,0 +1,14 @@
+package fun.seterain3913.notruenews.backend.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class News {
+    Integer id;
+    String title;
+    String content;
+}
