afb44938d0b0fe27b934c1bf4e03ff45eec49010
rov_waff <2710182206@qq.com>
2025-05-09 19:38:24 +0800

    init backend

---
+++ backend/src/test/java/fun/seterain3913/notruenews/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.seterain3913.notruenews.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
